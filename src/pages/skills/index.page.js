74a34b43e3b8d7701c74737bc3c024a9d105c204
Robinson Roa <roa.robinson97@gmail.com>
2024-03-14 15:27:04 -0400

    delete slug

--- src/pages/skills/index.page.js
+++ src/pages/skills/index.page.js
@@ -1,4 +1,3 @@
-import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 import readingTime from 'reading-time';
@@ -7,29 +6,3 @@ import { formatTimecode } from 'utils/timecode';
 
 export { Skills as default } from './Skills';
 
-export function getStaticProps() {
-  const allPosts = postFilePaths.map(filePath => {
-    const source = fs.readFileSync(path.join(POSTS_PATH, filePath));
-    const { data, content } = matter(source);
-
-    const { time } = readingTime(content);
-    const timecode = formatTimecode(time);
-
-    return {
-      ...data,
-      timecode,
-      slug: 'resume'
-    };
-  });
-
-
-  const posts = allPosts
-    .sort((a, b) => {
-      return new Date(a.date).getTime() - new Date(b.date).getTime();
-    })
-    .reverse();
-
-  return {
-    props: { posts },
-  };
-}
